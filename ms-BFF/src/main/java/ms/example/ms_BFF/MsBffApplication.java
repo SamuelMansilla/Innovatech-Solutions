f79a60dd7ba274ffd7e4590ce7fa4d612714ccbb
SB- Alumno <SB-Alumno@duoc.acad>
2026-05-29 10:43:17 -0400

    feat(bff): implementar cliente feign, circuit breaker y documentacion tecnica

--- ms-BFF/src/main/java/ms/example/ms_BFF/MsBffApplication.java
+++ ms-BFF/src/main/java/ms/example/ms_BFF/MsBffApplication.java
@@ -2,7 +2,9 @@ package ms.example.ms_BFF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsBffApplication {
 
